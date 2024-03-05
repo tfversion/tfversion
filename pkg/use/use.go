ee2d6ab666b4f710f8dbce2678f5cc125168328c
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-03-05 17:31:16 +0100

    Fix path check condition

--- pkg/use/use.go
+++ pkg/use/use.go
@@ -34,7 +34,7 @@ func UseVersion(versionOrAlias string) {
 
 	// inform the user that they need to update their PATH
 	path := os.Getenv("PATH")
-	if strings.Contains(path, useLocation) {
+	if !strings.Contains(path, useLocation) {
 		fmt.Printf("%s not found in your shell PATH\n", color.CyanString(useLocation))
 		fmt.Printf("Please run %s to make this version available in your shell\n", color.CyanString("`export PATH=%s:$PATH`", useLocation))
 		fmt.Printf("Additionally, consider adding this line to your shell profile (e.g., .bashrc, .zshrc or fish config) for persistence.\n")
