e64ec5819fcf9c927356011bb2ff26745e17d9f5
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-03-08 15:00:04 +0100

    Show nicer error when version does not exist (#31)
    * Show nicer error when version does not exist
    
    * Add very first unit test
    
    * Fix test env

---
+++ pkg/download/download_test.go
@@ -0,0 +1,23 @@
+package download
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGetDownloadLocation(t *testing.T) {
+	tempDir := t.TempDir()
+	t.Setenv("HOME", tempDir)
+	downloadLocation := GetDownloadLocation()
+
+	expectedLocation := filepath.Join(tempDir, ".tfversion", "versions")
+	if downloadLocation != expectedLocation {
+		t.Errorf("Expected download location %s, but got %s", expectedLocation, downloadLocation)
+	}
+
+	_, err := os.Stat(downloadLocation)
+	if os.IsNotExist(err) {
+		t.Errorf("Download location directory does not exist: %s", downloadLocation)
+	}
+}
