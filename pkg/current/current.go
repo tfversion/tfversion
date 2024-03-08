a9f5ec68ff3abaf7ca13c61b61f5e75f4ba61754
Mathijs van Mourick <14082160+mmourick@users.noreply.github.com>
2024-03-08 11:23:49 +0100

    refactor: simplify determining current active version

--- pkg/current/current.go
+++ pkg/current/current.go
@@ -1,11 +1,9 @@
 package current
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
-	"regexp"
 
 	"github.com/tfversion/tfversion/pkg/download"
 	"github.com/tfversion/tfversion/pkg/helpers"
@@ -25,14 +23,7 @@ func CheckCurrentVersion() {
 	if err != nil {
 		helpers.ExitWithError("could not determine active Terraform version", err)
 	}
-	left := fmt.Sprintf("%s/", download.VersionsDir)
-	right := fmt.Sprintf("/%s", download.TerraformBinaryName)
-	rx := regexp.MustCompile(`(?s)` + regexp.QuoteMeta(left) + `(.*?)` + regexp.QuoteMeta(right))
-	match := rx.FindStringSubmatch(realPath)
+	_, currentVersion := filepath.Split(filepath.Dir(realPath))
 
-	if match == nil {
-		// helpers.er
-		helpers.ExitWithError("could not determine active Terraform version", errors.New("failed to match regex on path"))
-	}
-	fmt.Printf("Current active Terraform version: %s\n", helpers.ColoredVersion(match[1]))
+	fmt.Printf("Current active Terraform version: %s\n", helpers.ColoredVersion(currentVersion))
 }
