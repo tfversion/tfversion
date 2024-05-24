86cc414c14ee2aaf0506239a75ffa58c75384ece
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-05-24 20:07:04 +0200

    Remove unneeded protection when deleting alias

--- pkg/unalias/unalias.go
+++ pkg/unalias/unalias.go
@@ -9,14 +9,9 @@ import (
 
 // Unalias removes the symlink for the specified alias.
 func Unalias(aliasName string) {
-	if !store.IsAlias(aliasName) {
-		return
-	}
-
 	err := store.RemoveSymlink(store.GetAliasPath(aliasName))
 	if err != nil {
 		helpers.ExitWithError("removing symlink", err)
 	}
-
 	fmt.Printf("Removed alias %s\n", helpers.ColoredVersion(aliasName))
 }
